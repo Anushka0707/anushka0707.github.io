ab9e0e8e0e0e1a5eeb847e8c400f705429b79edb
Anushka Singhee <144204659+Anushka0707@users.noreply.github.com>
2026-08-01 00:13:56 +1000

    Delete site-config.js

--- site-config.js
+++
@@ -1,6 +0,0 @@
-window.PORTFOLIO_CONFIG = {
-  githubUsername: "Anushka0707",
-  email: "anushkasinghee07@gmail.com",
-  linkedInUrl: "https://www.linkedin.com/in/anushka-singhee-106a98243/",
-  resumeUrl: "https://drive.google.com/file/d/1umYr-tup8Mn1JpuRewM5di90_QHgh-jh/view",
-};
